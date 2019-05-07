7a1ae0e3fa990df5e8c7fa6af44c51cbb01961da
Cairo Ramos <cairo.ramosoliveira4@gmail.com>
2019-05-07 11:26:38 -0300

    Features finais

--- start/routes.js
+++ start/routes.js
@@ -24,3 +24,8 @@ Route.get('/', () => {
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+    .apiOnly()
+    .middleware('auth')
+Route.get('images/:path', 'ImageController.show')
+Route.post('properties/:id/images', 'ImageController.store').middleware('auth')
